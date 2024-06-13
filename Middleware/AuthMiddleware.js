4d5fa08a8616317c6fbb3c7a2365df6ea8fcceba
Sid-80 <siddharthbramhecha@gmail.com>
2024-06-13 14:22:15 +0530

    fix: asynchandler

--- Middleware/AuthMiddleware.js
+++ Middleware/AuthMiddleware.js
@@ -1,5 +1,5 @@
 import { User } from "../Models/UserModel.js";
-import { asyncHandler } from "../utils/AsyncHandler.js";
+import { asyncHandler } from "../Utils/AsyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
